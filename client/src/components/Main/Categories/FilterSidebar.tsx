a8aed79dc7f6b26e1385f42c1e0ff1e8acacec43
Christina Lambiase <christina.lambiase@gmail.com>
2023-09-12 13:51:49 -0700

    color filters COMPLETE

--- client/src/components/Main/Categories/FilterSidebar.tsx
+++ client/src/components/Main/Categories/FilterSidebar.tsx
@@ -1,109 +1,63 @@
 import React, { useContext } from "react";
 import {
-  IconButton,
   Box,
-  CloseButton,
   Flex,
   useColorModeValue,
   Text,
-  Drawer,
-  DrawerContent,
-  useDisclosure,
-  BoxProps,
-  FlexProps,
   Stack,
   Checkbox,
 } from "@chakra-ui/react";
-import { FiMenu } from "react-icons/fi";
 import { ColorContext } from "../../../context/ColorProvider";
 
-export default function FilterSidebar() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  return (
-    <Box
-      minH="100vh"
-      bg={useColorModeValue("gray.100", "gray.900")}
-      mt={"45px"}
-    >
-      <SidebarContent
-        onClose={() => onClose}
-        display={{ base: "none", md: "block" }}
-      />
-      <Drawer
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        returnFocusOnClose={false}
-        onOverlayClick={onClose}
-        size="full"
-      >
-        <DrawerContent>
-          <SidebarContent onClose={onClose} />
-        </DrawerContent>
-      </Drawer>
-      {/* mobilenav */}
-      <MobileNav display={{ base: "flex", md: "none" }} onOpen={onOpen} />
-      <Box ml={{ base: 0, md: 60 }} p="0">
-        {/* Content */}
-      </Box>
-    </Box>
-  );
-}
-
-interface SidebarProps extends BoxProps {
-  onClose: () => void;
-}
-
-const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
+export default function FilterSidebar({
+  setSelectedColorIds,
+  selectedColorIds,
+}: {
+  setSelectedColorIds: (colorIds: number[]) => void;
+  selectedColorIds: number[];
+}) {
   const { colors } = useContext(ColorContext);
   return (
-    <Box
-      bg={useColorModeValue("white", "gray.900")}
-      borderRight="1px"
-      borderRightColor={useColorModeValue("gray.200", "gray.700")}
-      w={{ base: "full", md: 60 }}
-      pos="fixed"
-      h="full"
-      {...rest}
-    >
-      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        <Text fontSize="xl" fontWeight={600} mt={10}>
-          Filter by:
-        </Text>
-        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
-      </Flex>
-      <Text>Color</Text>
-      <Stack spacing={2} ml={10} direction="column">
-        {colors.map((color) => {
-          return <Checkbox colorScheme="gray">{color.color}</Checkbox>;
-        })}
-      </Stack>
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+      <Box
+        bg={useColorModeValue("white", "gray.900")}
+        borderRight="1px"
+        borderRightColor={useColorModeValue("gray.200", "gray.700")}
+        w={{ base: "full", md: 60 }}
+        pos="fixed"
+        h="full"
+      >
+        <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+          <Text fontSize="xl" fontWeight={600} mt={10}>
+            Filter by:
+          </Text>
+        </Flex>
+        <Text>Color</Text>
+        <Stack spacing={2} ml={10} direction="column">
+          {colors.map((color) => {
+            return (
+              <Checkbox
+                key={color.id} // Add a unique key
+                colorScheme="gray"
+                onChange={(e) => {
+                  let newSelectedColorIds = [...selectedColorIds];
+                  if (e.target.checked) {
+                    newSelectedColorIds.push(color.id);
+                  } else {
+                    newSelectedColorIds = newSelectedColorIds.filter(
+                      (id) => id !== color.id
+                    );
+                  }
+                  setSelectedColorIds(newSelectedColorIds);
+                }}
+              >
+                {color.color}
+              </Checkbox>
+            );
+          })}
+        </Stack>
+      </Box>
+      <Box ml={{ base: 0, md: 60 }} p="4"></Box>
     </Box>
   );
-};
-
-interface MobileProps extends FlexProps {
-  onOpen: () => void;
 }
-const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
-  return (
-    <Flex
-      ml={{ base: 0, md: 60 }}
-      px={{ base: 4, md: 24 }}
-      height="20"
-      alignItems="center"
-      bg={useColorModeValue("white", "gray.900")}
-      borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
-      justifyContent="flex-start"
-      {...rest}
-    >
-      <IconButton
-        variant="outline"
-        onClick={onOpen}
-        aria-label="open menu"
-        icon={<FiMenu />}
-      />
-    </Flex>
-  );
-};
--- client/src/components/Main/Categories/ShopAll.tsx
+++ client/src/components/Main/Categories/ShopAll.tsx
@@ -1,35 +1,47 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { StyleContext } from "../../../context/StyleProvider";
 import ProductCard from "../ProductCard";
 import { Flex, SimpleGrid, HStack } from "@chakra-ui/react";
 import FilterSidebar from "./FilterSidebar";
+import { ColorContext } from "../../../context/ColorProvider";
 
 export default function ShopAll() {
   const { styles } = useContext(StyleContext);
-  // console.log(styles);
+  const { colors } = useContext(ColorContext);
+  const [selectedColorIds, setSelectedColorIds] = useState<number[]>(
+    colors.map((color) => color.id)
+  );
+  const styleCards = styles
+    .filter((style) => {
+      const colorIds = style.skus.map((sku) => sku.color_id);
+      return (
+        selectedColorIds.length === 0 ||
+        colorIds.some((id) => selectedColorIds.includes(id))
+      );
+    })
+    .map((style) => {
+      const colorIds = style.skus.map((sku) => sku.color_id);
+      const images = style.skus.map((sku) => sku.image);
 
-  const styleCards = styles.map((style) => {
-    const colorIds = style.skus.map((sku) => sku.color_id);
-    const images = style.skus.map((sku) => sku.image);
-    // console.log(colorIds);
-    // console.log(images);
-
-    return (
-      <ProductCard
-        key={style.id}
-        id={style.id}
-        style_name={style.style_name}
-        price={style.price}
-        color_ids={colorIds}
-        images={images}
-      />
-    );
-  });
+      return (
+        <ProductCard
+          key={style.id}
+          id={style.id}
+          style_name={style.style_name}
+          price={style.price}
+          color_ids={colorIds}
+          images={images}
+        />
+      );
+    });
 
   return (
     <HStack justifyContent="flex-start">
       <Flex>
-        <FilterSidebar />
+        <FilterSidebar
+          setSelectedColorIds={setSelectedColorIds}
+          selectedColorIds={selectedColorIds}
+        />
         <SimpleGrid pr={"40"} columns={4} spacing={3} mt={20}>
           {styleCards}
         </SimpleGrid>
