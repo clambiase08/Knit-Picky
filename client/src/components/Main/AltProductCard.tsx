a85106e4fe78c98075642ce1bbcfb6b1bd5279c8
Christina Lambiase <christina.lambiase@gmail.com>
2023-09-13 15:54:27 -0700

    cleaned up commented out code

--- client/src/components/Main/AltProductCard.tsx
+++ client/src/components/Main/AltProductCard.tsx
@@ -10,16 +10,6 @@ import {
 import { ColorContext } from "../../context/ColorProvider";
 import { useHistory } from "react-router-dom";
 
-// const data = {
-//   isNew: true,
-//   imageURL:
-//     "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
-//   name: "Wayfarer Classic",
-//   price: 4.5,
-//   rating: 4.2,
-//   numReviews: 34,
-// };
-
 interface ProductCardProps {
   id: number;
   style_name: string;
